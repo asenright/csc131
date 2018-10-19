5c2fe1a8caec1a69fb92029f1b7ca4bcadad66b6
asenright <asenright@gmail.com>
2018-10-18 18:27:54 -0700

    Added vocabulary to operators.

--- src/Metrics.java
+++ src/Metrics.java
@@ -369,52 +369,70 @@ public class Metrics implements Runnable{
 		
 		//Halstead Metrics
 		if (calcHalstead && !currentlyBlockComment && codeLine.length() > 1) {
-			Set<String> keywords = new HashSet<String>();
+			Set<String> excludedOperands = new HashSet<String>(), includedOperators = new HashSet<String>();
+			
 			String operatorsRegex;
 			String operandsRegex;
 			switch (ext) {
 				case ".java" :
 				case ".javah" : 
-					keywords = new HashSet<String>(Arrays.asList("one", "of",
+					excludedOperands = new HashSet<String>(Arrays.asList("one", "of",
 							"abstract", "continue", "for", "new", "switch",
 							"assert", "default", "if", "package", "synchronized",
-							"boolean", "do", "goto", "private", "this",
-							"break", "double", "implements", "protected", "throw",
-							"byte", "else", "import", "public", "throws",
+							"do", "goto", "private", "this",
+							"break",  "implements", "protected", "throw",
+							 "else", "import", "public", "throws",
 							"case", "enum", "instanceof", "return", "transient",
-							"catch", "extends", "int", "short", "try",
-							"char", "final", "interface", "static", "void",
-							"class", "finally", "long", "strictfp", "volatile",
-							"const", "float", "native", "super", "while"));
+							"catch", "extends",  "try",
+							"final", "interface", "static", "void",
+							"class", "finally","strictfp", "volatile",
+							"const", "native", "super", "while"));
+					includedOperators = new HashSet<String>(
+							Arrays.asList("break","case","continue","default","do","if","else","enum","for","goto","if","new","return","asm","operator","private",
+								"protected","public","sizeof","struct","switch","union","while","this","namespace","using","try","catch","throw","abstract","concrete",
+								"const_cast","static_cast","dynamic_cast","reinterpret_cast","typeid","template","explicit","true","false","typename"));
 					break;
 				case ".c":
 				case ".h":
-					keywords = new HashSet<String>(Arrays.asList("auto","break","case","char","const","continue","default","do","double","else"	,"enum"	,"extern","float"	,"for"
-							,"goto"	,"if","int"	,"long"	,"register"	,"return","short",
+					excludedOperands = new HashSet<String>(Arrays.asList("auto","break","case","const","continue","default",
+							"do","else"	,"enum"	,"extern","for"
+							,"goto"	,"if","register","return",
 							"signed","sizeof","static","struct"	,"switch"	,"typedef"	,"union",
 							"unsigned","void"	,"volatile"	,"while"));
+					includedOperators = new HashSet<String>(
+							Arrays.asList("auto","extern","register","static","typedef","const","final",
+									 "volatile","break","case","continue",
+									 "default","do","if","else","enum","for","goto","if","new",
+									 "return","sizeof","struct","switch","union","while"));
 					break;
 				
 				case ".cpp":
 				case ".hpp":
-					keywords = new HashSet<String>(Arrays.asList("one", "of",
+					excludedOperands = new HashSet<String>(Arrays.asList("one", "of",
 							"abstract", "continue", "for", "new", "switch",
 							"assert", "default", "if", "package", "synchronized",
-							"boolean", "do", "goto", "private", "this",
-							"break", "double", "implements", "protected", "throw",
-							"byte", "else", "import", "public", "throws",
+							"do", "goto", "private", "this",
+							"break", "implements", "protected", "throw",
+							"else", "import", "public", "throws",
 							"case", "enum", "instanceof", "return", "transient",
-							"catch", "extends", "int", "short", "try",
-							"char", "final", "interface", "static", "void",
-							"class", "finally", "long", "strictfp", "volatile",
-							"const", "float", "native", "super", "while"));
+							"catch", "extends",  "short", "try",
+							"final", "interface", "static", "void",
+							"class", "finally", "strictfp", "volatile",
+							"const",  "native", "super", "while"));
+					includedOperators = new HashSet<String>(
+							Arrays.asList("auto","extern","register","static","typedef","virtual",
+									"mutable","inline","const","friend","volatile","final","break",
+									"case","continue","default","do","if","else","enum","for","goto",
+									"if","new","return","asm","operator","private","protected","public",
+									"sizeof","struct","switch","union","while","this","namespace","using",
+									"try","catch","throw","abstract","concrete","const_cast","static_cast",
+									"dynamic_cast","reinterpret_cast","typeid","template","explicit","true","false","typename"));
 					break;
 			}
-			
-		    operatorsRegex = "/?/?";
-		    		//"(=)|(>)|(<)|(!)|(~)|(/?)|(:)|(==)|(<=)|(>=)|(!=)|(&&)|(/|/|)|(/+/+)|(--)|(/+)|(-)|(/*)|(/)|(&)|(/|)|(^)|(%)|(<<)|(>>)|(>>>)|(/+=)|(-=)|(/*=)|(/=)|(&=)|(/|=)|(^=)|(%=)|(<<=)|(>>=)|(>>>=)"; 
+			operatorsRegex = "(=)|(>)|(<)|(!)|(~)|(/?)|(:)|(==)|(<=)|(>=)|(!=)|(&&)|(/|/|)|(/+/+)|(--)|(/+)|(-)|(/*)|(/)|(&)|(/|)|(^)|(%)|(<<)|(>>)|(>>>)|(/+=)|(-=)|(/*=)|(/=)|(&=)|(/|=)|(^=)|(%=)|(<<=)|(>>=)|(>>>=)";
 		    //Keywords and ops regex shamelessly borrowed from https://www.daniweb.com/programming/software-development/threads/307653/halstead-metrics
 		    operandsRegex = "(\\w+)|(\\d+)";
+		    for (String word : includedOperators) operandsRegex += "|(" + word + ")";
 
 		    Matcher operatorsMatcher = Pattern.compile(operatorsRegex).matcher(codeLine);
 		    Matcher operandsMatcher = Pattern.compile(operandsRegex).matcher(codeLine);
@@ -428,7 +446,11 @@ public class Metrics implements Runnable{
 			   }
 			  while (operandsMatcher.find()) {
 				   String group = operandsMatcher.group();
-			       if (group.length() > 0  && !keywords.contains(group)) {
+				   if (includedOperators.contains(group)) {
+					   uniqueOperators.add(group);
+					   nodeTotalOperators++;
+				   }
+			       if (group.length() > 0  && !excludedOperands.contains(group)) { // per doc referenced in Design, reserved words are not operands for H-metrics
 				       uniqueOperands.add(group);
 				       nodeTotalOperands++;
 				  	}
@@ -442,9 +464,10 @@ public class Metrics implements Runnable{
 				effort = volume > 0 ? difficulty/volume : 0;
 				time = effort / 18;
 				bugs = volume > 0 ? (volume / 3000) :  0 ;
-				//System.out.println(	"Code: " + codeLine + 
-				//		"\n	Operators: " + uniqueOperators.toString() + 
-				//		"\n	Operands: " + uniqueOperands.toString());
+				
+				System.out.println(	"Code: " + codeLine + 
+						"\n	Operators: " + uniqueOperators.toString() + 
+				     	"\n	Operands: " + uniqueOperands.toString());
 		}
 		
 	
--- test.c
+++ test.c
@@ -3,13 +3,13 @@
 *
 * Yeah, I commented ALL this crap out. Didn't need it I guess!
 */
-
+static const int n = var + 2;
 This is (an abstract test && file)
 /*So much comments!*/
 
 It tests = things || stuff /*Like this!*/
 /*It is not compilable c code.*/ It shouldn't be compilable; if it were longer then we'd have trouble
-int n = var + 2;
+
 It DOES have 6 lines of source and 10 lines of comment though. n++; //10? Seems like a lot!
 ///////////////////////////////////There's a lot of // here but still no code.
 This last line might be kinda tricky: 5/7 would agree
