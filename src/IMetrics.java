65cf4a3a866801b2707935a701cb06b0ccf58431
asenright <asenright@gmail.com>
2018-10-21 21:45:36 -0700

    Added IMetrics interface and calls

--- src/IMetrics.java
+++ src/IMetrics.java
@@ -1,6 +1,5 @@
 
 interface IMetrics {
-
     boolean setPath(String path);   // sets the file path to process
                                     // returns true if current path is valid
     boolean isSource();             // returns true if the file is a source file
--- src/Metrics.java
+++ src/Metrics.java
@@ -22,7 +22,7 @@ import picocli.CommandLine.*;
 
 /*Wrapper class for Metrics*/
 @Command(description="Prints metrics of the given file to STDOUT.",	name="Metrics")
-public class Metrics implements Runnable{
+public class Metrics implements IMetrics, Runnable{
 	private static final int COLUMN_SEP_WIDTH = 1; //Amount of whitespace to put between columns.
 	private static final String COLUMN_LINES = "lines", 
 								COLUMN_WORDS = "words",
@@ -72,6 +72,7 @@ public class Metrics implements Runnable{
 		totalBugs, totalTime;
 
 	private LinkedList<metricsFileNode> listHead = null;	
+	private metricsFileNode item = null;	
 	
 	public static void main(String[] args) {		
 		Metrics wordCounterInstance = new Metrics();
@@ -101,7 +102,87 @@ public class Metrics implements Runnable{
 		for (metricsFileNode lastListItem : listHead) formattedPrint(lastListItem);
 		if (listHead.size() > 1) 
 			formattedPrint(this);
-	}
+		}
+	
+	    public boolean setPath(String path) {
+	    	try {
+	    		this.item = new metricsFileNode(new File(path));
+	    	} catch (Exception e) {
+	    		System.out.println("Could not create metricsFileNode");
+	    		e.printStackTrace();
+	    	}
+	    	return true;
+	    }
+		    // returns true if current path is valid
+	    public boolean isSource() {
+			return (item.ext.equals(".c")||
+					item.ext.equals(".h")||
+					item.ext.equals(".cpp")||
+					item.ext.equals(".hpp")||
+					item.ext.equals(".java")||
+					item.ext.equals(".javah"));
+		};             // returns true if the file is a source file
+		
+		// basic counts for any file
+		//
+		 public int getLineCount() {
+			return item.lines;
+		};
+		 public int getWordCount() {
+			 return item.words;
+		 }
+		 public int getCharacterCount() {
+			 return item.chars;
+		 };
+		
+		// source code line counts
+		//
+		 public int getSourceLineCount() {
+			 return item.linesOfCode;
+		 };
+		 public int getCommentLineCount() {
+			 return item.linesOfComment;
+		 };
+		
+		// Halstead metrics
+		//
+		 public int getHalsteadn1() {
+			 return item.nodeTotalOperands;
+		 };            // number of distinct operands
+		 public int getHalsteadn2() {
+			 return item.nodeTotalOperators;
+		 };            // number of distinct operators
+		 public int getHalsteadN1() {
+			 return item.uniqueOperands == null ? 0 : item.uniqueOperands.size();
+		 };            // number of operands
+		 public int getHalsteadN2(){
+			 return item.uniqueOperators == null ? 0 : item.uniqueOperators.size();
+		 };            // number of operators
+		
+		 public int getHalsteadVocabulary() {
+			 return item.vocabulary;
+		 };
+		 public int getHalsteadProgramLength() {
+			 return item.length;
+		 };
+		 public int getHalsteadCalculatedProgramLenght() {
+			 return item.calcLength;
+		 };
+		 public int getHalsteadVolume() {
+			 return item.volume;
+		 };
+		 public int getHalsteadDifficulty() {
+			 return item.difficulty;
+		 };
+		 public int getHalsteadEffort() {
+			 return item.effort;
+		 };
+		 public int getHalsteadTime() {
+			 return item.time;
+		 };
+		 public int getHalsteadBugs() {
+			 return item.bugs;
+		 };
 	
 	/** Gathers total metrics from specified list.
 	 * @param fileList List of items to iterate over.
