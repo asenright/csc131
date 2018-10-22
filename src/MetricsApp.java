43e19f734cdb14bbfc990c7f9305dda76af16c75
Andrew Enright <asenright@gmail.com>
2018-10-22 11:29:15 -0700

    Separated church and state; Metrics is now just backend and MetricsApp is strictly frontend

--- src/MetricsApp.java
+++ src/MetricsApp.java
@@ -1,4 +1,268 @@
+/*	Metrics.java
 
-public class MetricsApp {
+*	A java implementation of the unix 'wc' utility by Andrew Enright.
+*
+*	Usage: 'java wc <-l|-c|-w|-s|-C> filename
+*
+*  Credit to technicalkeeda.com for getFileExtension()	
+*  Credit to https://www.geeksforgeeks.org/print-unique-words-string/ for unique words in string
+*/
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import picocli.CommandLine;
+import picocli.CommandLine.*;
+
+/*Wrapper class for Metrics*/
+@Command(description="Prints metrics of the given file to STDOUT.",	name="Metrics")
+public class MetricsApp implements Runnable{
+	@Option(description = "Show usage", names = { "-h" }, paramLabel="help")
+	static boolean showHelp;
+	
+	@Option(description = "Count number of lines", names = { "-l" }, paramLabel="countLines") 
+	boolean countLines;
+	
+	@Option(description = "Count number of words", names = { "-w" }, paramLabel="countWords") 
+	boolean countWords;
+	
+	@Option(description = "Count number of chars", names = { "-c" }, paramLabel="countChars")
+	boolean countChars;
+	
+	@Option(description = "Count lines of source code", names = { "-s" }, paramLabel="countCode")
+	boolean countCode;
+
+	@Option(description = "Count lines of comment", names = { "-C" }, paramLabel="countComment")
+	boolean countComments;
+	
+	@Option(description = "Calculate Halstead complexity", names = {"-H"}, paramLabel="countHalstead")
+	boolean calcHalstead;
+	
+	@Parameters
+	LinkedList<String> filePaths;	
+	
+	private static final int COLUMN_SEP_WIDTH = 1; //Amount of whitespace to put between columns.
+	private static final String COLUMN_LINES = "lines", 
+								COLUMN_WORDS = "words",
+								COLUMN_CHARS = "chars",
+								COLUMN_SOURCE = "source",
+								COLUMN_COMMENTS = "comments",
+								COLUMN_OPERATORS = "operators",
+								COLUMN_OPERANDS = "operands",
+								COLUMN_UNIQUE_OPERATORS = "unqOperators",
+								COLUMN_UNIQUE_OPERANDS = "unqOperands",
+								COLUMN_VOCAB = "vocab",
+								COLUMN_LENGTH = "length",
+								COLUMN_CALC_LENGTH = "calcLength",
+								COLUMN_VOLUME = "volume",
+								COLUMN_DIFFICULTY = "difficulty",
+								COLUMN_EFFORT = "effort",
+								COLUMN_BUGS = "estBugs",
+								COLUMN_TIME = "estTime";
+	
+	private int totalLines, totalChars, totalWords, totalCode, totalComments, totalUniqueOperands, 
+				totalUniqueOperators, totalOperands, totalOperators,
+				totalEffort, totalVocab, totalLength, totalCalcLength, totalVolume, totalDifficulty, 
+				totalBugs, totalTime;
+	private LinkedList<IMetrics> listHead = null;	
+
+
+	public static void main(String[] args) {		
+		MetricsApp metricsAppInstance = new MetricsApp();
+		if (args.length == 0 || showHelp ) {
+			CommandLine.usage(metricsAppInstance, System.err);
+			System.exit(0);
+		}
+		CommandLine.run(metricsAppInstance, args);
+	}	
+	
+	public void run() {		
+		//No argument specified? Print all metrics
+		if (!countLines && !countWords && !countChars && !countCode && !countComments) 			
+			countLines = countWords = countChars = countCode = countComments = calcHalstead = true ;	
+		
+		listHead = new LinkedList<IMetrics>();
+		
+		try { populateMetricsList(filePaths, listHead); }
+		catch (Exception e) { 
+			System.out.println("Error encountered populating list : " + e.getMessage());
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		gatherFileMetrics(listHead);	
+		printHeader();
+		for (IMetrics lastListItem : listHead) formattedPrint(lastListItem);
+		if (listHead.size() > 1) 
+			formattedPrint(this);
+		}
+	
+	/** Gathers total metrics from specified list.
+	 * @param fileList List of items to iterate over.
+	 */
+	private void gatherFileMetrics(LinkedList<IMetrics> fileList) {
+		for (IMetrics lastListItem : fileList) {
+			totalWords += lastListItem.getWordCount();
+			totalChars += lastListItem.getCharacterCount();
+			totalLines += lastListItem.getLineCount();
+			totalCode += lastListItem.getSourceLineCount();
+			totalComments += lastListItem.getCommentLineCount();
+			totalOperators += lastListItem.getHalsteadn2();
+			totalOperands += lastListItem.getHalsteadn1();
+			totalUniqueOperators += lastListItem.getHalsteadN1();
+			totalUniqueOperands += lastListItem.getHalsteadN2();
+			totalEffort  += lastListItem.getHalsteadEffort();
+			totalVocab += lastListItem.getHalsteadVocabulary();
+			totalLength += lastListItem.getHalsteadProgramLength();
+			totalCalcLength  += lastListItem.getHalsteadCalculatedProgramLenght();
+			totalVolume += lastListItem.getHalsteadVolume();
+			totalDifficulty  += lastListItem.getHalsteadDifficulty();
+			totalBugs += lastListItem.getHalsteadBugs();
+			totalTime += lastListItem.getHalsteadTime();
+		}
+	}
+	
+	
+
+	/** Populates a linkedList of MetricsFileNodes with the given List of filePaths.
+	 * @param filePaths List of filePaths.
+	 * @param listHead List of MetricsFileNodes.
+	 * @throws FileNotFoundException Throws if file not found.
+	 */
+	private void populateMetricsList(List<String> filePaths, LinkedList<IMetrics> listHead) throws FileNotFoundException {
+		for (String current : filePaths) {
+				IMetrics tmp = new Metrics();
+				tmp.setPath(current);
+				listHead.add(tmp);
+			}	
+	}
+
+	/**
+	 * Prints output header.
+	 */
+	private void printHeader() {
+		if (countLines) printHeader(COLUMN_LINES, totalLines);
+		if (countWords) printHeader(COLUMN_WORDS, totalWords);
+		if (countChars) printHeader(COLUMN_CHARS, totalChars);
+		if (countCode)  printHeader(COLUMN_SOURCE, totalCode);
+		if (countComments) printHeader(COLUMN_COMMENTS, totalComments);
+		if (calcHalstead) {
+			printHeader(COLUMN_OPERATORS, totalOperators);	
+			printHeader(COLUMN_OPERANDS, totalOperands);	
+			printHeader(COLUMN_UNIQUE_OPERATORS, totalUniqueOperators);	
+			printHeader(COLUMN_UNIQUE_OPERANDS, totalUniqueOperands);	
+		
+			printHeader(COLUMN_VOCAB, totalVocab);	
+			printHeader(COLUMN_LENGTH, totalLength);	
+			printHeader(COLUMN_CALC_LENGTH, totalCalcLength);	
+			printHeader(COLUMN_VOLUME, totalVolume);	
+			printHeader(COLUMN_DIFFICULTY, totalDifficulty);	
+			printHeader(COLUMN_EFFORT, totalEffort);	
+			printHeader(COLUMN_TIME, totalTime);	
+			printHeader(COLUMN_BUGS, totalBugs);	
+		}
+		System.out.printf(" %-20s%n", "filename");
+	}
+
+	
+	/**
+	 * Master print method, used for printing individual node metrics as well as the total.
+	 */
+	private void formattedPrint(Integer lines, Integer words, Integer chars, Integer linesCode, Integer linesComment, 
+						Integer operators, Integer operands, Integer uniqueOperators, Integer uniqueOperands,
+						Integer vocab, Integer length, Integer totalCalcLength, Integer volume, Integer difficulty, Integer effort,
+						Integer bugs, Integer time,
+						String filename) {
+		if (countLines) printMetric(COLUMN_LINES, lines);
+		if (countWords) printMetric(COLUMN_WORDS, words);
+		if (countChars) printMetric(COLUMN_CHARS, chars);
+		
+		if (countCode) printCodeMetric(COLUMN_SOURCE, linesCode);
+		if (countComments) printCodeMetric(COLUMN_COMMENTS, linesComment);
+		if (calcHalstead) {
+			printCodeMetric(COLUMN_OPERATORS, operators);
+			printCodeMetric(COLUMN_OPERANDS, operands);
+			printCodeMetric(COLUMN_UNIQUE_OPERATORS, uniqueOperators);
+			printCodeMetric(COLUMN_UNIQUE_OPERANDS, uniqueOperands);
+
+			printCodeMetric(COLUMN_VOCAB, vocab);
+			printCodeMetric(COLUMN_LENGTH, length);
+			printCodeMetric(COLUMN_CALC_LENGTH, totalCalcLength);
+			printCodeMetric(COLUMN_VOLUME, volume);
+			printCodeMetric(COLUMN_DIFFICULTY, difficulty);
+			printCodeMetric(COLUMN_EFFORT, effort);
+			
+			printCodeMetric(COLUMN_TIME, time);
+			printCodeMetric(COLUMN_BUGS, bugs);
+			
+		}
+		System.out.printf(" %s%n", filename);
+	}
+
+	/** Abstraction method for neatness.
+	 * @param toPrint Node whose metrics we want to print.
+	 */
+	private void formattedPrint(IMetrics toPrint) {
+		formattedPrint(toPrint.getLineCount(), 
+					toPrint. getWordCount(), 
+					toPrint.getCharacterCount(), 
+					toPrint.getSourceLineCount(), 
+					toPrint.getCommentLineCount(), 
+					toPrint.getHalsteadn2(), 
+					toPrint.getHalsteadn1(), 
+					toPrint.getHalsteadN2(),
+					toPrint.getHalsteadN1(),
+					toPrint.getHalsteadVocabulary(),
+					toPrint.getHalsteadProgramLength(), 
+					toPrint.getHalsteadCalculatedProgramLenght(), 
+					toPrint.getHalsteadVolume(), 
+					toPrint.getHalsteadDifficulty(), 
+					toPrint.getHalsteadEffort(),
+					toPrint.getHalsteadBugs(), 
+					toPrint.getHalsteadTime(), 
+					((Metrics) toPrint).item.file.getPath());
+	}
+	
+	
+	/** Abstraction method for neatness. Used for printing totals.
+	 * @param toPrint Metrics object whose totals are to be printed.
+	 */
+	private void formattedPrint(MetricsApp toPrint) {
+		formattedPrint(toPrint.totalLines, toPrint.totalWords, toPrint.totalChars, toPrint.totalCode, toPrint.totalComments, 
+				toPrint.totalOperators, toPrint.totalOperands, toPrint.totalUniqueOperators, toPrint.totalUniqueOperands,
+				toPrint.totalVocab, toPrint.totalLength, toPrint.totalCalcLength, toPrint.totalVolume, toPrint.totalDifficulty, 
+				toPrint.totalEffort, toPrint.totalBugs, toPrint.totalTime,
+				"total" );
+}
+	
+	/** Printing helper method for managing column width.
+	 * @param minimumWidth Narrowest column width returnable, usually the label width
+	 * @param totalToMeasure Integer in column
+	 * @return
+	 */
+	private int getColumnWidth(int minimumWidth, int totalToMeasure) {		
+		return Math.max(new Integer(totalToMeasure).toString().length(), minimumWidth) + COLUMN_SEP_WIDTH;
+	}
+	
+	private void printHeader(String columnName, int metricTotal) {
+		System.out.printf("%"+ getColumnWidth(columnName.length() , metricTotal) +"s", columnName);
+	}
+	private void printMetric(String columnName, int metric) {
+		System.out.printf("%"+ getColumnWidth(columnName.length(), metric) + "s", new Integer(metric).toString());
+	}
+
+	private void printCodeMetric(String columnName, int metric) {
+		System.out.printf("%"+ getColumnWidth(columnName.length(), metric) + "s", metric > 0 ? new Integer(metric).toString() : "");
+	}
+	
+
+		
+	/** Metrics File Node class. Give it a file and it'll automatically accumulate appropriate metrics.
+	 */
 }
---
+++ src/MetricsFileNode.java
@@ -0,0 +1,235 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MetricsFileNode {
+	
+	int lines, chars, words, linesOfCode, linesOfComment, nodeTotalOperators, nodeTotalOperands,
+		vocabulary, length, calcLength, volume, difficulty, effort, time, bugs;
+	Set<String> uniqueOperators = new HashSet<String>(), uniqueOperands = new HashSet<String>();
+	protected File file;
+	String ext = "";
+	private boolean currentlyBlockComment = false;
+	
+	public MetricsFileNode(File toCount) {
+		this.file = toCount;
+		if (!file.exists()) throw new IllegalArgumentException("No file found matching '" + file.getName() + "'");
+		
+		ext = getFileExtension(file);
+		String line = null;
+		
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while (reader.read() > -1)
+				chars++;
+			reader.close();
+			
+			reader = new BufferedReader(new FileReader(file)); //Reset reader to start reading lines
+			line = reader.readLine();
+			while (line != null) { 		
+				getLineMetrics(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Argument could not be parsed as an argument or filename.");
+			System.exit(1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("IO Exception while reading.");
+			System.exit(1);
+		}
+	}
+	private void getLineMetrics(String line) {
+		lines++;		
+		words += line.split("\\s+").length; 
+		if (	!line.isEmpty() && 
+				(ext.equals(".c") || 
+				ext.equals(".java") || 
+				ext.equals(".cpp") || 
+				ext.equals(".h") || 
+				ext.equals(".hpp"))) {		
+				getCodeLineMetrics(line);
+		}
+	}
+	private void getCodeLineMetrics(String line) {
+			String codeLine = "";
+			//Count code and Comments
+			if (currentlyBlockComment && !line.contains("*/")) linesOfComment++; 
+				else if (line.contains("/*")) { //Does this line have a self-contained comment /*like this*/
+					linesOfComment++; 		
+					String before = line.substring(0, line.indexOf('*') - 1).trim(),
+							after = "";
+					if (line.contains("*/")) {
+						after = line.substring(line.lastIndexOf("*/") + 2).trim();
+					} else currentlyBlockComment = true;
+					if (before.length() > 1) { 
+						linesOfCode++; //Are there contents before the comment?
+						codeLine = before;
+					}
+					else if (after.length() > 2) {
+						codeLine = after;
+						linesOfCode++; //Are there contents after the comment?
+					}
+			} else if (line.contains("*/")) {
+					linesOfComment++;
+					currentlyBlockComment = false;
+					String after = line.substring(line.lastIndexOf("*/") + 2).trim();
+					if (after.length() > 2) {
+						codeLine = after;
+						linesOfCode++; //Are there contents after the comment?
+					}
+			}
+			else if (line.contains("//")) {
+				linesOfComment++;
+				String before = line.substring(0, line.indexOf("//")).trim();
+						
+				if (before.length() > 1) {
+					codeLine = before;
+					linesOfCode++;
+				}
+			} else  {
+				linesOfCode++;	
+				codeLine = line;
+			}
+	
+	
+	//Halstead Metrics
+	if (!currentlyBlockComment && codeLine.length() > 1) {
+		Set<String> excludedOperands = new HashSet<String>(), includedOperators = new HashSet<String>();
+		
+		String operatorsRegex;
+		String operandsRegex;
+		switch (ext) {
+			case ".java" :
+			case ".javah" : 
+				excludedOperands = new HashSet<String>(Arrays.asList("one", "of",
+						"abstract", "continue", "for", "new", "switch",
+						"assert", "default", "if", "package", "synchronized",
+						"do", "goto", "private", "this",
+						"break",  "implements", "protected", "throw",
+						 "else", "import", "public", "throws",
+						"case", "enum", "instanceof", "return", "transient",
+						"catch", "extends",  "try",
+						"final", "interface", "static", "void",
+						"class", "finally","strictfp", "volatile",
+						"const", "native", "super", "while"));
+				includedOperators = new HashSet<String>(
+						Arrays.asList("break","case","continue","default","do","if","else","enum","for","goto","if","new","return","asm","operator","private",
+							"protected","public","sizeof","struct","switch","union","while","this","namespace","using","try","catch","throw","abstract","concrete",
+							"const_cast","static_cast","dynamic_cast","reinterpret_cast","typeid","template","explicit","true","false","typename"));
+				break;
+			case ".c":
+			case ".h":
+				excludedOperands = new HashSet<String>(Arrays.asList("auto","break","case","const","continue","default",
+						"do","else"	,"enum"	,"extern","for"
+						,"goto"	,"if","register","return",
+						"signed","sizeof","static","struct"	,"switch"	,"typedef"	,"union",
+						"unsigned","void"	,"volatile"	,"while"));
+				includedOperators = new HashSet<String>(
+						Arrays.asList("auto","extern","register","static","typedef","const","final",
+								 "volatile","break","case","continue",
+								 "default","do","if","else","enum","for","goto","if","new",
+								 "return","sizeof","struct","switch","union","while"));
+				break;
+			
+			case ".cpp":
+			case ".hpp":
+				excludedOperands = new HashSet<String>(Arrays.asList("one", "of",
+						"abstract", "continue", "for", "new", "switch",
+						"assert", "default", "if", "package", "synchronized",
+						"do", "goto", "private", "this",
+						"break", "implements", "protected", "throw",
+						"else", "import", "public", "throws",
+						"case", "enum", "instanceof", "return", "transient",
+						"catch", "extends",  "short", "try",
+						"final", "interface", "static", "void",
+						"class", "finally", "strictfp", "volatile",
+						"const",  "native", "super", "while"));
+				includedOperators = new HashSet<String>(
+						Arrays.asList("auto","extern","register","static","typedef","virtual",
+								"mutable","inline","const","friend","volatile","final","break",
+								"case","continue","default","do","if","else","enum","for","goto",
+								"if","new","return","asm","operator","private","protected","public",
+								"sizeof","struct","switch","union","while","this","namespace","using",
+								"try","catch","throw","abstract","concrete","const_cast","static_cast",
+								"dynamic_cast","reinterpret_cast","typeid","template","explicit","true","false","typename"));
+				break;
+		}
+		operatorsRegex = "(=)|(>)|(<)|(!)|(~)|(/?)|(:)|(==)|(<=)|(>=)|(!=)|(&&)|(/|/|)|(/+/+)|(--)|(/+)|(-)|(/*)|(/)|(&)|(/|)|(^)|(%)|(<<)|(>>)|(>>>)|(/+=)|(-=)|(/*=)|(/=)|(&=)|(/|=)|(^=)|(%=)|(<<=)|(>>=)|(>>>=)";
+	    //Keywords and ops regex shamelessly borrowed from https://www.daniweb.com/programming/software-development/threads/307653/halstead-metrics
+	    operandsRegex = "(\\w+)|(\\d+)";
+	    for (String word : includedOperators) operandsRegex += "|(" + word + ")";
+
+	    Matcher operatorsMatcher = Pattern.compile(operatorsRegex).matcher(codeLine);
+	    Matcher operandsMatcher = Pattern.compile(operandsRegex).matcher(codeLine);
+	
+		  while (operatorsMatcher.find()) {
+			  	String group = operatorsMatcher.group();
+			  	if (group.length() > 0) {
+			       uniqueOperators.add(group);
+			       nodeTotalOperators++;
+			  	}
+		   }
+		  while (operandsMatcher.find()) {
+			   String group = operandsMatcher.group();
+			   if (includedOperators.contains(group)) {
+				   uniqueOperators.add(group);
+				   nodeTotalOperators++;
+			   }
+		       if (group.length() > 0  && !excludedOperands.contains(group)) { // per doc referenced in Design, reserved words are not operands for H-metrics
+			       uniqueOperands.add(group);
+			       nodeTotalOperands++;
+			  	}
+		   }
+	
+			vocabulary = uniqueOperators.size() + uniqueOperands.size();
+			length = nodeTotalOperators + nodeTotalOperands;
+			calcLength = uniqueOperators.size() * log2(uniqueOperators.size()) + uniqueOperands.size()*log2(uniqueOperands.size());
+			volume = nodeTotalOperators * log2(vocabulary);
+			difficulty = uniqueOperands.size() > 0 ? ((uniqueOperators.size() / 2) * (nodeTotalOperands / uniqueOperands.size()) ): 0;
+			effort = difficulty*volume;
+			time = effort / 18;
+			bugs = volume > 0 ? (volume / 3000) :  0 ;
+			
+			//System.out.println(	"Code: " + codeLine + 
+			//		"\n	Operators: " + uniqueOperators.toString() + 
+			//     	"\n	Operands: " + uniqueOperands.toString());
+	}
+	
+
+	}
+	
+	private int log2(double n) {
+		return (int) Math.round(Math.log(n)/Math.log(2));
+	}
+	
+	/**Credit to technicalkeeda.com for getFileExtension()
+	 * @param file File to get extension from.
+	 * @return Returns the extension of the file.
+	 */
+	private String getFileExtension(File file) {
+		String extension = "";
+		try {
+		if (file != null && file.exists()) {
+			String name =file.getName();
+			extension = name.substring(name.lastIndexOf("."));
+		}
+		return extension;
+		} catch (Exception e) {
+			extension = "";
+		}
+		return extension;
+	}
+
+}
